4483e3c3325e1d944dffe669dbdfc4a1214cf1f8
ten8mystery <146376897+ten8mystery@users.noreply.github.com>
2026-02-07 21:52:27 -0800

    Update uv.config.js

--- data/local/pages/unlocker/ultraviolet/uv/uv.config.js
+++ data/local/pages/unlocker/ultraviolet/uv/uv.config.js
@@ -1,7 +1,7 @@
 self.__uv$config = {
     prefix: '/s/',
     bare: 'https://bare.benroxy.com/bare/', 
-    wisp: 'wss://wisp.mercurywork.shop/', 
+    wisp: 'wss://glseries.net/wisp/', 
     transport: '/data/local/pages/unlocker/ultraviolet/uv/uv.bundle.js',
     handler: '/data/local/pages/unlocker/ultraviolet/uv/uv.handler.js',
     bundle: '/data/local/pages/unlocker/ultraviolet/uv/uv.bundle.js',
